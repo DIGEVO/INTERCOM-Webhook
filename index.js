d9d62637518d013c9af78dbbbd8ffc4a3826db25
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-10-30 00:00:45 -0500

    testing1

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ require('dotenv').config();
 const router = require('./router');
 const controllers = require('./controllers');
 
-router.subscribe('POST', '/', controllers.processMessageFromIntercom);
-router.subscribe('GET', '/.*', (req, res) => {
-    res.statusCode = 200; res.end();
-});
+// router.subscribe('POST', '/', controllers.processMessageFromIntercom);
+// router.subscribe('GET', '/.*', (req, res) => {
+//     res.statusCode = 200; res.end();
+// });
+
+// const server = http.createServer();
+// server.on('request', router.route);
+// server.listen(process.env.PORT || 1338);
 
 const server = http.createServer();
-server.on('request', router.route);
+server.on('request', (req, res) => {
+    console.log(`---< ${req.method} ${req.url}`);
+    res.statusCode = 200; 
+    res.end();
+});
 server.listen(process.env.PORT || 1338);
