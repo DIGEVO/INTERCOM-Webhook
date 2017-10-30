5672eb82dd214644e942e17ece0046ae59e03d7b
Rigoberto Leander Salgado Reyes <rlsalgado2006@@gmail.com>
2017-10-29 21:26:54 -0500

    adding default home page

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const router = require('./router');
 const controllers = require('./controllers');
 
 router.subscribe('POST', '/', controllers.processMessageFromIntercom);
+router.subscribe('GET', '/.*', (req, res) => {
+    res.statusCode = 200; res.end();
+});
 
 const server = http.createServer();
 server.on('request', router.route);
