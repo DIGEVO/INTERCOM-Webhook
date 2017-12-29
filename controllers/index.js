859d5af6a21648a2e7d35c3daac60a55841c2400
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2017-12-28 21:52:00 -0500

    removing comments

--- controllers/index.js
+++ controllers/index.js
@@ -7,19 +7,9 @@ const directline = require('../libs/directline');
 
 const self = module.exports = {
     processMessageFromIntercom: (req, res) => {
-        console.log(`constroller: ${JSON.stringify(req.body)}`);
-        console.log('*******************************************************');
-
         res.statusCode = 200;
         res.end();
         directline.connectBot(self.createMessage(req.body)).catch(e => console.error(e));
-        // new Promise((resolve, reject) => {
-        //     res.statusCode = 200;
-        //     res.end();
-        //     resolve(true);
-        // })
-        //     .then(() => directline.connectBot(self.createMessage(req.body)))
-        //     .catch(e => console.error(e));
     },
 
     createMessage: reqBody =>
@@ -30,7 +20,6 @@ const self = module.exports = {
                 .then(r => ({
                     paused: reqBody.topic === 'conversation.admin.replied',
                     userId: reqBody.data.item.user.user_id,
-                    //   conversationId: r.body.custom_attributes.conversationId,
                     text: reqBody.data.item.conversation_parts.conversation_parts[0].body
                 }))
                 .catch(e => console.error(e))
