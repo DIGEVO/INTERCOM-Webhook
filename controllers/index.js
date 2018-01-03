81bd293c56d50cf6af2ac42c705ea0a4b27a60dd
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2018-01-02 23:57:02 -0500

    testing 1

--- controllers/index.js
+++ controllers/index.js
@@ -11,7 +11,7 @@ const self = module.exports = {
 
     processMessageFromIntercom: (req, res) => {
 
-      //  console.log(`topic: ${req.body.topic} text: ${req.body.data.item.conversation_parts.conversation_parts[0].body}`);
+        //  console.log(`topic: ${req.body.topic} text: ${req.body.data.item.conversation_parts.conversation_parts[0].body}`);
 
         res.statusCode = 200;
         res.end();
@@ -20,26 +20,66 @@ const self = module.exports = {
 
     createMessage: reqBody => {
 
-        let text = null;
+        // let text = null;
 
-        if (reqBody.topic === 'conversation.admin.replied') {
-            text = reqBody.data.item.conversation_parts.conversation_parts[0].body ?
+        // if (reqBody.topic === 'conversation.admin.replied') {
+        //     text = reqBody.data.item.conversation_parts.conversation_parts[0].body ?
+        //         reqBody.data.item.conversation_parts.conversation_parts[0].body
+        //             .replace(/<p>/g, '')
+        //             .replace(/<\/p>/g, '')
+        //             .replace(/<br>/g, '\n') :
+        //         null
+        // }
+
+        // return !['conversation.admin.replied', 'conversation.admin.closed'].some(s => s === reqBody.topic) ?
+        //     Promise.resolve(undefined) :
+        //     new Intercom.Client({ token: process.env.TOKEN }).users
+        //         .find({ id: reqBody.data.item.user.id })
+        //         .then(r => ({
+        //             paused: reqBody.topic === 'conversation.admin.replied',
+        //             userId: reqBody.data.item.user.user_id,
+        //             text: text
+        //         }))
+        //         .catch(e => console.error(e));
+
+        //
+        const partType = reqBody.data.item.conversation_parts.conversation_parts[0].part_type;
+        if (reqBody.topic === 'conversation.admin.replied' && partType === 'comment') {
+            const text = text = reqBody.data.item.conversation_parts.conversation_parts[0].body ?
                 reqBody.data.item.conversation_parts.conversation_parts[0].body
                     .replace(/<p>/g, '')
                     .replace(/<\/p>/g, '')
                     .replace(/<br>/g, '\n') :
-                null
-        }
+                null;
 
-        return !['conversation.admin.replied', 'conversation.admin.closed'].some(s => s === reqBody.topic) ?
-            Promise.resolve(undefined) :
-            new Intercom.Client({ token: process.env.TOKEN }).users
+            return new Intercom.Client({ token: process.env.TOKEN }).users
                 .find({ id: reqBody.data.item.user.id })
                 .then(r => ({
-                    paused: reqBody.topic === 'conversation.admin.replied',
+                    paused: true,
                     userId: reqBody.data.item.user.user_id,
                     text: text
                 }))
-                .catch(e => console.error(e))
+                .catch(e => console.error(e));
+        }
+
+        if (reqBody.topic === 'conversation.admin.closed') {
+            const text = text = reqBody.data.item.conversation_parts.conversation_parts[0].body ?
+                reqBody.data.item.conversation_parts.conversation_parts[0].body
+                    .replace(/<p>/g, '')
+                    .replace(/<\/p>/g, '')
+                    .replace(/<br>/g, '\n') :
+                null;
+
+            return new Intercom.Client({ token: process.env.TOKEN }).users
+                .find({ id: reqBody.data.item.user.id })
+                .then(r => ({
+                    paused: false,
+                    userId: reqBody.data.item.user.user_id,
+                    text: text
+                }))
+                .catch(e => console.error(e));
+        }
+
+        return Promise.resolve(undefined);
     }
 };
