f1197521adb8fdcd5fda33ff7c942e85d49aab79
Rigoberto Leander Salgado Reyes <rlsalgado2006@gmail.com>
2018-02-08 22:04:31 -0500

    filtering by admin id

--- controllers/index.js
+++ controllers/index.js
@@ -6,36 +6,38 @@ require('dotenv').config();
 const directline = require('../libs/directline');
 
 const self = module.exports = {
-    processMessageFromIntercom: (req, res) => {
-        res.statusCode = 200;
-        res.end();
-        directline.connectBot(self.createMessage(req.body)).catch(e => console.error(e));
-    },
+	processMessageFromIntercom: (req, res) => {
+		res.statusCode = 200;
+		res.end();
+		directline.connectBot(self.createMessage(req.body)).catch(e => console.error(e));
+	},
 
-    createMessage: reqBody => {
-        const partType = reqBody.data.item.conversation_parts.conversation_parts[0].part_type;
-	   
+	createMessage: reqBody => {
+		const partType = reqBody.data.item.conversation_parts.conversation_parts[0].part_type;
+		const botAdminId = reqBody.data.item.conversation_parts.conversation_parts[0].author.id;
+
 		console.log(JSON.stringify(reqBody));
 		console.log('---------------------------------------------------------');
 
-        if ((reqBody.topic === 'conversation.admin.replied' && partType === 'comment') || reqBody.topic === 'conversation.admin.closed') {
-            const text = reqBody.data.item.conversation_parts.conversation_parts[0].body ?
-                reqBody.data.item.conversation_parts.conversation_parts[0].body
-                    .replace(/<p>/g, '')
-                    .replace(/<\/p>/g, '')
-                    .replace(/<br>/g, '\n') :
-                null;
+		if ((reqBody.topic === 'conversation.admin.replied' && partType === 'comment' && botAdminId !== process.env.INTERCOM_BOT_ADMIN_ID) ||
+			reqBody.topic === 'conversation.admin.closed') {
+			const text = reqBody.data.item.conversation_parts.conversation_parts[0].body ?
+				reqBody.data.item.conversation_parts.conversation_parts[0].body
+				.replace(/<p>/g, '')
+				.replace(/<\/p>/g, '')
+				.replace(/<br>/g, '\n') :
+				null;
 
-            return new Intercom.Client({ token: process.env.TOKEN }).users
-                .find({ id: reqBody.data.item.user.id })
-                .then(r => ({
-                    paused: reqBody.topic === 'conversation.admin.replied',
-                    userId: reqBody.data.item.user.user_id,
-                    text: text
-                }))
-                .catch(e => console.error(e));
-        }
+			return new Intercom.Client({token: process.env.TOKEN}).users
+				.find({id: reqBody.data.item.user.id})
+				.then(r => ({
+					paused: reqBody.topic === 'conversation.admin.replied',
+					userId: reqBody.data.item.user.user_id,
+					text: text
+				}))
+				.catch(e => console.error(e));
+		}
 
-        return Promise.resolve(undefined);
-    }
+		return Promise.resolve(undefined);
+	}
 };
